7dff07192feb5b6452bb1d60b173587c96a76328
x3nl1 <stepanagapov2005@gmail.com>
2026-05-12 18:57:48 +0300

    feat: register Driver router in FastAPI application

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.routers.health import router as health_router
 from app.api.passenger import router as passenger_router
+from app.api.driver import router as driver_router
 
 app = FastAPI(
     title="Taxi Service API",
@@ -11,6 +12,7 @@ app = FastAPI(
 
 app.include_router(health_router)
 app.include_router(passenger_router)
+app.include_router(driver_router)
 
 
 @app.get("/")
